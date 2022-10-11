7a8160ce006b00103fd18ec219832c40098e2ff6
Sergey Simenshteyn <simenshteyn@gmail.com>
2022-10-11 18:42:46 +0700

    Fix location json representation

--- main_service/src/main/java/ru/practicum/explorewithme/event/Location.java
+++ main_service/src/main/java/ru/practicum/explorewithme/event/Location.java
@@ -1,8 +1,15 @@
 package ru.practicum.explorewithme.event;
 
+import lombok.*;
+
 import javax.persistence.*;
 
 @Embeddable
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Location {
     private float lat;
     private float lon;
